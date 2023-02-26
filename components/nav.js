4c7d77de464e8226384f7d35bed613c6af03a56b
Istiaq Ahmed <2021462@iub.edu.bd>
2023-02-27 04:32:19 +0600

    2

--- components/nav.js
+++ components/nav.js
@@ -1,75 +1,89 @@
-import { BsFillMoonStarsFill } from "react-icons/bs";
-import { FaDownload} from 'react-icons/fa';
-import Link from "next/link";
-import { useState } from "react";
+import { BsFillMoonStarsFill, BsList } from "react-icons/bs";
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Nav({ darkMode, setDarkMode }) {
-  const [showMenu, setShowMenu] = useState(false);
-
-  const handleMenuToggle = () => {
-    setShowMenu(!showMenu);
-  }
-
+  const [navbar, setNavbar] = useState(false);
   return (
-    <nav className="py-10 mb-12 flex justify-between dark:text-white fixed top-0 left-10 right-10 z-50">
-      <Link href="/">
-        <a className="font-burtons text-2xl">Istiaq Ahmed</a>
-      </Link>
-      <div className="flex items-center">
-        <button
-          className="block sm:hidden cursor-pointer text-2xl mr-8"
-          onClick={handleMenuToggle}
-        >
-          <span className="sr-only">Toggle menu</span>
-          <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-            <path
-              className={`${showMenu ? 'hidden' : 'block'}`}
-              d="M4 6h16M4 12h16M4 18h16"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              className={`${showMenu ? 'block' : 'hidden'}`}
-              d="M6 18L18 6M6 6l12 12"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </button>
-        <ul
-          className={`${showMenu ? 'block' : 'hidden'} sm:block sm:flex sm:items-center`}
-        >
-          <li>
-            <Link href="/Resume">
-              <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md sm:ml-8 mt-4 sm:mt-0">
-                Resume
-              </a>
+    <div className={darkMode ? "dark" : ""}>
+      <nav className="w-full fixed top-10 left-10 right-0 z-50">
+        <div className="justify-between px-4 mx-auto md:items-center md:flex md:px-8">
+          <div className="">
+            <Link href="/">
+              <a className="font-burtons text-2xl dark:text-white">Istiaq Ahmed</a>
             </Link>
-          </li>
-          <li>
-            <Link href="/Projects">
-              <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md sm:ml-8 mt-4 sm:mt-0">
-                Projects
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/Contact">
-              <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md sm:ml-8 mt-4 sm:mt-0">
-                Contact me
-              </a>
-            </Link>
-          </li>
-        </ul>
-        <li style={{ position: 'fixed', bottom: '20px', right: '40px' }}>
+          </div>
+          <div className="mr-10 hidden md:block">
+            <ul className="flex items-center justify-end">
+              <li>
+                <Link href="/Resume">
+                  <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md sm:ml-8 mt-4 sm:mt-0">
+                    Resume
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/Projects">
+                  <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md sm:ml-8 mt-4 sm:mt-0">
+                    Projects
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/Contact">
+                  <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md sm:ml-8 mt-4 sm:mt-0">
+                    Contact me
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="fixed top-10 right-10 md:hidden">
+            <button
+              className=" dark:text-white rounded-md outline-none focus:border-gray-400 focus:border"
+              onClick={() => setNavbar(!navbar)}
+            >
+              <BsList size={24} />
+            </button>
+          </div>
+          <div
+            className={`justify-self-center pb-3 md:hidden ${navbar ? 'block' : 'hidden'}`}
+            style={{ position: 'absolute', top: '100%', right: 10, zIndex: 1 }}
+          >
+            <div className="bg-gray-800 mr-20 rounded-lg shadow-lg">
+              <ul className="items-center justify-center md:flex flex-col">
+                <li>
+                  <Link href="/Resume">
+                    <a className="text-gray-200 flex items-center py-2 px-4 block hover:text-cyan-500" onClick={() => setNavbar(!navbar)}>
+                      Resume
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/Projects">
+                    <a className="text-gray-200 flex items-center py-2 px-4 block hover:text-cyan-500" onClick={() => setNavbar(!navbar)}>
+                      Projects
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/Contact">
+                    <a className="text-gray-200 flex items-center py-2 px-4 block hover:text-cyan-500" onClick={() => setNavbar(!navbar)}>
+                      Contact me
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <li class="dark:text-white" style={{ position: 'fixed', bottom: '20px', right: '40px' }}>
           <BsFillMoonStarsFill
             onClick={() => setDarkMode(!darkMode)}
             className=" cursor-pointer text-2xl"
           />
         </li>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
